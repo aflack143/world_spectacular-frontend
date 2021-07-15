8b17a731f644337c5e3d27d5aa6db269dd8e45b4
Andrea Clark <andrea_clark@glic.com>
2021-07-15 13:32:29 -0500

    fixed 'sub' issue - working on update/delete btn

--- world_spectacular/src/components/UserVisit/UserVisit.js
+++ world_spectacular/src/components/UserVisit/UserVisit.js
@@ -6,15 +6,16 @@ import axios from 'axios';
 const UserVisit = (props) => {
 
     const { user } = useAuth0();
-    const { sub } = user;
+    const sub = user;
 
     const [userVisitedCountries, setUserVisitedCountries] = useState(sub)
     // const [userVisitedCountries, setUserVisitedCountries] = useState('')
   
     const findUserCountries = async (e) => { 
+        e.preventDefault()
         // const { sub } = user;
-        // setUserVisitedCountries(sub)
-        const userVisited = await axios(`http://localhost:8000/profiles/${userVisitedCountries}/visited/`)  
+        setUserVisitedCountries(userVisitedCountries.sub)
+        const userVisited = await axios(`http://localhost:8000/profiles/${userVisitedCountries.sub}/visited/`)  
         console.log(userVisited);
         // const userVisitedCountries = userVisited.map(visited => {
         //     return (visited)
@@ -25,20 +26,22 @@ const UserVisit = (props) => {
 
 
     const updateCountryVisit = async () => {
-        const countryVisit = await axios(`http://localhost:8000/profiles/${userVisitedCountries}/${props.countryId}/`)  
-        console.log(countryVisit.data)
+        const countryVisit = await axios.post(`http://localhost:8000/profiles/${userVisitedCountries.sub}/${props.countryId}/`)  
+        console.log(countryVisit)
         // const updatedVisit = countryVisit.map(visited => {
         //     return (visited)
         // })
+        window.location.reload()
     }
 
-    // useEffect (() => {
-    //     findUserCountries()
+    // useEffect (async () => {
+    //     await setUserVisitedCountries(sub)
+    //     await findUserCountries()
     // }, [])
-    // console.log(user)
+    console.log(userVisitedCountries.sub)
     return (
         <div>
-            <button onClick={() => findUserCountries}>Add to Visited Countries</button>
+            <button onClick={ async() => await findUserCountries}>Add to Visited Countries</button>
             {/* <button onClick={() => 'test'}>Add to Dream Visit Countries</button> */}
         </div>
     );
